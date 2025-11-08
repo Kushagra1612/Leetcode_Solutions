ecd2b34ec836a3586f281c6d573c9cea55204539
Kushagra1612 <kushagraverma1234@gmail.com>
2025-11-08 17:03:43 +0530

    Time: 162 ms (80.39%), Space: 12.6 MB (91.2%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if (n <= 2) {
+            return 0;
+        }
+
+        std::vector<bool> is_prime(n, true);
+        is_prime[0] = false;
+        is_prime[1] = false;
+
+        for (int i = 2; i * i < n; ++i) {
+            if (is_prime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    is_prime[j] = false;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 2; i < n; ++i) {
+            if (is_prime[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
