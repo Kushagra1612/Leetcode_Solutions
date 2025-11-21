03785306c243215c7f96ec7bfba057efb5848df4
Kushagra1612 <kushagraverma1234@gmail.com>
2025-11-21 09:44:30 +0530

    Time: 48 ms (47.9%), Space: 93.9 MB (87.64%) - LeetHub

---
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    static bool comp(const vector<int>&a, const vector<int>&b){
+        return a[1]<b[1];
+    }
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+       if(intervals.size() == 0) return 0;
+
+       sort(intervals.begin(),intervals.end(),comp);
+
+        int cnt=1;
+        int lastEndTime=intervals[0][1];
+        for(int i=0;i<intervals.size();i++)
+        {
+            if(intervals[i][0] >=lastEndTime){
+                cnt=cnt+1;
+                lastEndTime=intervals[i][1];
+            }
+        }
+        return intervals.size()-cnt;
+    }
+};
