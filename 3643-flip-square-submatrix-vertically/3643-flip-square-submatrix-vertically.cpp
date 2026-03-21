d1e685797296e7501bf0387f656d71b848380653
Kushagra Verma <kushagraverma1234@gmail.com>
2026-03-22 00:13:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3643-flip-square-submatrix-vertically/3643-flip-square-submatrix-vertically.cpp
+++ 3643-flip-square-submatrix-vertically/3643-flip-square-submatrix-vertically.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> reverseSubmatrix(vector<vector<int>>& grid, int x, int y, int k) {
-        for(int i=0;i<k;i++){
-            for(int j=0;j<k>>1;j++){
-                swap(grid[x+j][y+i],grid[x+k-j-1][y+i]);
+         int half = k / 2; //bitwise shift
+
+        for (int col = 0; col < k; col++) {
+            int top = x;
+            int bottom = x + k - 1;
+
+            for (int row = 0; row < half; row++) {
+                swap(grid[top][y + col], grid[bottom][y + col]);
+                top++;
+                bottom--;
             }
         }
+
         return grid;
     }
 };
