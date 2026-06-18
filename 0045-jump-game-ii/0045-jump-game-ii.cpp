9ceddfc22b2b3ca3d168a4ae0bc94d203fffc9c2
Kushagra Verma <kushagraverma1234@gmail.com>
2026-06-18 22:53:34 +0530

    Time: 0 ms (100%), Space: 20.5 MB (42.81%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -4,11 +4,11 @@ public:
         int n=nums.size();
         int jumps=0,currentEnd=0,farthest=0;
         for(int i=0;i<n-1;++i){
-           farthest = max(farthest, i + nums[i]);
-           if(i==currentEnd){
-            jumps++;
-            currentEnd=farthest;
-           }
+            farthest=max(farthest,i+nums[i]);
+        if(i==currentEnd){
+        jumps++;
+        currentEnd=farthest;
+        }
         }
         return jumps;
     }
