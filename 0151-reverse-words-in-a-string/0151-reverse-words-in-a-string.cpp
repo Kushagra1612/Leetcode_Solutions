bc7267c5db3986aa5d298a284580b87a5b689d0b
Kushagra Verma <kushagraverma1234@gmail.com>
2026-07-14 13:59:19 +0530

    Time: 5 ms (16.38%), Space: 10.2 MB (62.46%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -6,16 +6,16 @@ public:
         int l=0,r=0;
         int n=s.length();
         while(i<n){
-            while(i<n && s[i] !=' '){
-                s[r++]=s[i++];
-            }
-            if(l<r){
-                reverse(s.begin()+l,s.begin()+r);
+            while(i<n && s[i]!=' '){
+            s[r++]=s[i++];
+        }
+        if(l<r){
+             reverse(s.begin()+l,s.begin()+r);
                 s[r]=' ';
                 r++;
                 l=r;
-            }
-            i++;
+        }
+        i++;
         }
         s=s.substr(0,r-1);
         return s;
