d9966f89bd8ba57ce23f2f1522389c627a1c5fe7
Kushagra Verma <kushagraverma1234@gmail.com>
2026-07-15 11:56:27 +0530

    Time: 0 ms (100%), Space: 8.5 MB (46.67%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
-    int gcd(int x,int y){
-        return y==0?x:gcd(y,x%y);
-    }
-
     int gcdOfOddEvenSums(int n) {
-     return gcd(n*n,n*(n+1));
+        return n;
     }
 };
