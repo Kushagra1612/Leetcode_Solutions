0bb4e8e6e3095002ad0fe92d0315886913a0a0fb
Kushagra Verma <kushagraverma1234@gmail.com>
2026-08-13 18:27:11 +0530

    Time: 0 ms (100%), Space: 8.6 MB (67.68%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -3,10 +3,8 @@ public:
     bool isSubsequence(string s, string t) {
         int m=s.size();
         int n=t.size();
-        int i=0;
-        int j=0;
-
-        while(i<m && j<n){
+        int i=0,j=0;
+        while(i<m && j<n) {
             if(s[i]==t[j]){
                 i++;
             }
