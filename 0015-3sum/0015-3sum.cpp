e1c79e2f225bbccd918f37d20b1cd9dbd77e0962
Kushagra Verma <kushagraverma1234@gmail.com>
2026-08-25 22:49:39 +0530

    Time: 42 ms (85.9%), Space: 29.2 MB (46.28%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,42 +1,37 @@
 class Solution {
 public:
+   vector<vector<int>> threeSum(vector<int>& nums) {
     vector<vector<int>> result;
-    void twoSum(vector<int>& nums,int target,int i,int j){
-        while(i<j){
-        if(nums[i]+nums[j]>target){
-            j--;
-        }
-       else if(nums[i]+nums[j]<target){
-            i++;
-        }
-        else{
-            while(i<j && nums[i]==nums[i+1]) i++;
-            while(i<j && nums[j]==nums[j-1]) j--;
-        
-        result.push_back({-target,nums[i],nums[j]});
-        i++;
-        j--;
-    }
-    }
-}
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        int n=nums.size();
-        
-        if(n<3)
-        return {};
+    sort(nums.begin(), nums.end());
+    int n = nums.size();
 
-        result.clear(); 
-        sort(nums.begin(),nums.end());
+    for (int i = 0; i < n - 2; i++) {
+        // No three positive numbers can sum to zero
+        if (nums[i] > 0) break;
 
-        for(int i=0;i<n;i++){
-            if(i>0 && nums[i]==nums[i-1]){
-                continue;
+        // Skip duplicate values for the first element
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+        int left = i + 1, right = n - 1;
+
+        while (left < right) {
+            int sum = nums[i] + nums[left] + nums[right];
+
+            if (sum < 0) {
+                left++;
+            } else if (sum > 0) {
+                right--;
+            } else {
+                result.push_back({nums[i], nums[left], nums[right]});
+                // Skip duplicates for both pointers
+                while (left < right && nums[left] == nums[left + 1]) left++;
+                while (left < right && nums[right] == nums[right - 1]) right--;
+                left++;
+                right--;
             }
-            int n1=nums[i];
-            int target=-n1;
-
-            twoSum(nums,target,i+1,n-1);
         }
-        return result;
     }
+
+    return result;
+}
 };
