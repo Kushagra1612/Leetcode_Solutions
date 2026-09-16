318bb89af9564355616bd4e33ad4bddb4a4dd639
Kushagra Verma <kushagraverma1234@gmail.com>
2026-09-17 00:16:48 +0530

    Time: 46 ms (52.04%), Space: 19.4 MB (39.3%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,28 +1,17 @@
-#include <string>
-#include <unordered_set>
-#include <algorithm>
-
 class Solution {
 public:
-    int lengthOfLongestSubstring(std::string s) {
-        if (s.empty()) {
-            return 0;
-        }
-
-        std::unordered_set<char> charSet;
-        int maxLen = 0;
-        int left = 0;
-
-        for (int right = 0; right < s.length(); ++right) {
-            while (charSet.find(s[right]) != charSet.end()) {
-                charSet.erase(s[left]);
-                left++;
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char,int> lastIndex;
+        int left=0;
+        int maxSum=0;
+        for(int right=0;right<s.size();right++){
+            char c=s[right];
+            if(lastIndex.count(c) && lastIndex[c] >=left){
+                left=lastIndex[c]+1;
             }
-
-            charSet.insert(s[right]);
-            maxLen = std::max(maxLen, right - left + 1);
+            lastIndex[c]=right;
+            maxSum=max(maxSum,right-left+1);
         }
-
-        return maxLen;
+        return maxSum;
     }
 };
