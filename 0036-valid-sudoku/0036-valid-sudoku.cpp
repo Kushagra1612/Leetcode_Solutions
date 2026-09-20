e15d6c7b4e60888acbeb82879d00eb6f5820d98e
Kushagra Verma <kushagraverma1234@gmail.com>
2026-09-20 16:07:21 +0530

    Time: 2 ms (51.26%), Space: 25.9 MB (7.84%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        unordered_set<char> rows[9];
-        unordered_set<char> cols[9];
-        unordered_set<char> boxes[9];
+        unordered_set<int> rows[9],cols[9],boxes[9];
 
         for(int r=0;r<9;r++){
             for(int c=0;c<9;c++){
-                if(board[r][c] == '.'){
-                    continue;
-                }
-                char value=board[r][c];
-                 int boxIndex = (r / 3) * 3 + (c / 3);
+                if(board[r][c]=='.') continue;
 
-                if (rows[r].count(value) || cols[c].count(value) || boxes[boxIndex].count(value)) {
-                    return false;
-                }
+                char digit=board[r][c];
+                int boxIdx=(r/3)*3+(c/3);
 
-                rows[r].insert(value);
-                cols[c].insert(value);
-                boxes[boxIndex].insert(value);
+            if(rows[r].count(digit) || cols[c].count(digit) || boxes[boxIdx].count(digit)){
+                return false;
+            }
+
+            rows[r].insert(digit);
+            cols[c].insert(digit);
+            boxes[boxIdx].insert(digit);
             }
         }
         return true;
