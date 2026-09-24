48d02fe94672d70db178da024795816d3be6a581
Kushagra Verma <kushagraverma1234@gmail.com>
2026-09-24 19:42:51 +0530

    Time: 1489 ms (31.23%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -3,3 +3,17 @@ Select s.year,s.price,p.product_name
 from Sales s
 JOIN Product p
 ON s.product_id=p.product_id;
+
+-- SELECT 
+--     p.product_name, 
+--     s.year, 
+--     s.price
+-- FROM Sales s
+-- JOIN Product p USING (product_id);
+
+-- SELECT 
+--     p.product_name, 
+--     s.year, 
+--     s.price
+-- FROM Sales s, Product p
+-- WHERE s.product_id = p.product_id;
